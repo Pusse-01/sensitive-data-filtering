e0516bdb34a60664a0a59ec9271dcce4810f6f24
Nirodya Pussadeniya <en95142@sjp.ac.lk>
2023-07-05 09:02:56 +0530

    updated

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from streamlit_option_menu import option_menu
 import openai
 
 
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    selected = option_menu("Menu", ["Scan Text", 'Scan Files'], 
+        icons=['file-text', 'folder'], menu_icon="cast", default_index=1)
 
-
-# os.environ['OPENAI_API_KEY'] = ('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')
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-
-def scan_text_page(openai_api_key):
+def scan_text_page():
     st.title("Scan Text")
     text = st.text_input("Enter your text:")
     submit = st.button("Submit")
@@ -24,8 +24,8 @@ def scan_text_page(openai_api_key):
         findings = scan_text_spacy(text)
         # st.write("Filtered Text:")
         # st.write(findings)
-        st.subheader("After content filtering - this is what will be sent to ChatGPT:\n\n")
-        st.write( findings.text, "\n\n----\n\n")
+        # st.subheader("After content filtering - this is what will be sent to ChatGPT:\n\n")
+        # st.write( findings.text, "\n\n----\n\n")
 
         # Send prompt to OpenAI model for AI-generated response
         completion = openai.ChatCompletion.create(
@@ -40,10 +40,21 @@ def scan_text_page(openai_api_key):
             {"role": "user", "content": text}
         ]
         )
-        st.subheader("Here's a generated response with Normal Text:\n")
-        st.write( completion2['choices'][0].message.content)
-        st.subheader("Here's a generated response with Recacted Text:\n")
-        st.write( completion['choices'][0].message.content)
+        # st.subheader("Here's a generated response with Normal Text:\n")
+        res_with_normal_text = completion2['choices'][0].message.content
+        # st.write(res_with_normal_text )
+        # st.subheader("Here's a generated response with Recacted Text:\n")
+        res_with_redacted_text = completion['choices'][0].message.content
+        data = {
+            "Input Text": [text],
+            "Redacted Text": [findings.text],
+            "OpenAI Response (Normal Text)": [res_with_normal_text],
+            "OpenAI Response (Redacted Text)": [res_with_redacted_text]
+        } 
+        # Display the table
+        st.subheader("Generated Responses:")
+        st.table(data)
+        # st.write(res_with_redacted_text)
         # for finding in findings[0]:
         #     # print(finding[0].finding)
         #     # st.info(f"Finding text: {finding.finding},  Detector name: {finding.detector_name}", icon="ℹ️")
@@ -57,12 +68,9 @@ def scan_files_page():
     st.write("Scan Files Page")
 
 # 1. as sidebar menu
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    selected = option_menu("Menu", ["Scan Text", 'Scan Files'], 
-        icons=['file-text', 'folder'], menu_icon="cast", default_index=1)
+
 # selected
 if selected == "Scan Text":
-    scan_text_page(openai_api_key)
+    scan_text_page()
 elif selected == "Scan Files":
     scan_files_page()
--- utils.py
+++ utils.py
@@ -1,64 +1,157 @@
 # from nightfall import Confidence, RedactionConfig, DetectionRule, Detector, Nightfall
 import os
-from presidio_analyzer import AnalyzerEngine
+from presidio_analyzer import AnalyzerEngine, LocalRecognizer, RecognizerResult
 from presidio_anonymizer import AnonymizerEngine
+import re
+import spacy
 
-# def scan_text(text):
-#     # By default, the client reads the API key from the environment variable NIGHTFALL_API_KEY
-#     nightfall = Nightfall()
+def redact_money(text):
+    # Regular expression pattern to match money amounts
+    pattern = r'(?i)\b(?:\d+(?:[.,]\d+)?\s*(?:million|billion)?\s*(?:dollars|euros|rupees|rs|lkr|rs.)|(?:dollars|euros|rupees|rs|lkr|rs.)\s*\d+(?:[.,]\d+)?)\b'
 
-#     # A rule contains a set of detectors to scan with
-#     name = Detector(min_confidence=Confidence.POSSIBLE, nightfall_detector="PERSON_NAME", display_name="Name",
-#                                 redaction_config=RedactionConfig(
-#                                     remove_finding=False, 
-#                                     substitution_phrase="[name]"))
-#     email = Detector(min_confidence=Confidence.POSSIBLE, nightfall_detector="EMAIL_ADDRESS", display_name="Email",
-#                                 redaction_config=RedactionConfig(
-#                                     remove_finding=False, 
-#                                     substitution_phrase="[email]"))
-#     phone = Detector(min_confidence=Confidence.POSSIBLE, nightfall_detector="PHONE_NUMBER", display_name="Phone No",
-#                                 redaction_config=RedactionConfig(
-#                                     remove_finding=False, 
-#                                     substitution_phrase="[phones]"))
-#     passwords = Detector(min_confidence=Confidence.POSSIBLE, nightfall_detector="PASSWORD_IN_CODE", display_name="Password",
-#                                 redaction_config=RedactionConfig(
-#                                     remove_finding=False, 
-#                                     substitution_phrase="[password]"))
-#     cc = Detector(min_confidence=Confidence.POSSIBLE, nightfall_detector="CREDIT_CARD_NUMBER", display_name="Credit Card Number",
-#                                 redaction_config=RedactionConfig(
-#                                     remove_finding=False, 
-#                                     substitution_phrase="[Credit Card Number]"))
-    
+    # Find all matches of the pattern in the text
+    matches = re.findall(pattern, text)
 
-#     detection_rule = DetectionRule([
-#                                 name, email, phone, passwords,cc])
+    # Redact the matches in the text
+    redacted_text = text
+    for match in matches:
+        redacted_text = redacted_text.replace(match, '[MONEY]')
 
-#     findings, redacted_payload = nightfall.scan_text( [text], detection_rules=[detection_rule])
-#     # If the message has sensitive data, use the redacted version, otherwise use the original message
-#     print(redacted_payload)
-#     if redacted_payload[0]:
-#         message_body = redacted_payload[0]
-#     else:
-#         message_body = text
-#     return message_body
+    return redacted_text
 
-# print(findings)
+class MyFinancialRecognizer(LocalRecognizer):
+    def __init__(self):
+        self.supported_entities = ["MONEY", "PERCENT"]
+        self.supported_language = "en"
+        self.nlp = None
+        self.is_loaded = False
+        self._id = "custom_recognizer"
+        self.name = "My Financial Recognizer"
+        self.context = ''
+
+    def load(self):
+        # Load the spaCy model for English
+        self.nlp = spacy.load("en_core_web_sm")
+
+    def analyze(self, text, entities, nlp_artifacts):
+        if not self.nlp:
+            self.load()
+
+        # Process the text using spaCy
+        doc = self.nlp(text)
+
+        results = []
+
+        # Extract money amounts
+        for ent in doc.ents:
+            if ent.label_ == "MONEY":
+                results.append(
+                    RecognizerResult(
+                        entity_type="MONEY",
+                        start=ent.start_char,
+                        end=ent.end_char,
+                        score=1.0,
+                    )
+                )
+
+            # Extract percentages
+            elif ent.label_ == "PERCENT":
+                # Find the number preceding the percentage sign
+                prev_token = doc[ent.start - 1]
+                if prev_token.like_num:
+                    number = prev_token.text
+                    percent = ent.text
+                    combined_entity = number + percent
+                    results.append(
+                        RecognizerResult(
+                            entity_type="PERCENT",
+                            start=prev_token.idx,
+                            end=ent.end_char,
+                            score=1.0,  # Adjust the confidence score as needed
+                        ))
+
+        return results
+
+
+# def scan_text_spacy(text):
+#     # Set up the engine, loads the NLP module (spaCy model by default) 
+#     # and other PII recognizers
+#     analyzer = AnalyzerEngine()
+
+#     # Call analyzer to get results
+#     results = analyzer.analyze(text=text,
+#                             entities=[
+#                             "PHONE_NUMBER",
+#                             # "US_DRIVER_LICENSE",
+#                             # "US_PASSPORT",
+#                             "LOCATION",
+#                             "CREDIT_CARD",
+#                             # "CRYPTO",
+#                             # "UK_NHS",
+#                             # "US_SSN",
+#                             "US_BANK_NUMBER",
+#                             "EMAIL_ADDRESS",
+#                             # "DATE_TIME",
+#                             # "IP_ADDRESS",
+#                             "PERSON",
+#                             # "IBAN_CODE",
+#                             "NRP",
+#                             # "US_ITIN",
+#                             # "MEDICAL_LICENSE",
+#                             "URL"
+#                             ],
+#                             language='en')
+#     print(results)
+
+#     # Analyzer results are passed to the AnonymizerEngine for anonymization
+
+#     anonymizer = AnonymizerEngine()
+
+#     anonymized_text = anonymizer.anonymize(text=text,analyzer_results=results)
+
+#     return(anonymized_text)
 
 def scan_text_spacy(text):
-    # Set up the engine, loads the NLP module (spaCy model by default) 
-    # and other PII recognizers
+    # Set up the analyzer engine and registry
     analyzer = AnalyzerEngine()
+    registry = analyzer.registry
 
-    # Call analyzer to get results
-    results = analyzer.analyze(text=text,
-                            # entities=["PHONE_NUMBER"],
-                            language='en')
+    # Create an instance of your custom recognizer
+    my_recognizer = MyFinancialRecognizer()
+
+    # Add your custom recognizer to the registry
+    registry.add_recognizer(my_recognizer)
+
+    # Call the analyzer to get results
+    results = analyzer.analyze(
+        text=text,
+        entities=[
+                            "PHONE_NUMBER",
+                            # "US_DRIVER_LICENSE",
+                            # "US_PASSPORT",
+                            "LOCATION",
+                            "CREDIT_CARD",
+                            # "CRYPTO",
+                            # "UK_NHS",
+                            # "US_SSN",
+                            "US_BANK_NUMBER",
+                            "EMAIL_ADDRESS",
+                            # "DATE_TIME",
+                            # "IP_ADDRESS",
+                            "PERSON",
+                            # "IBAN_CODE",
+                            "NRP",
+                            # "US_ITIN",
+                            # "MEDICAL_LICENSE",
+                            "URL"
+                            ],
+        language="en",
+    )
     print(results)
 
-    # Analyzer results are passed to the AnonymizerEngine for anonymization
-
+    # Use the AnonymizerEngine for anonymization
     anonymizer = AnonymizerEngine()
+    anonymized_text = anonymizer.anonymize(text=text, analyzer_results=results)
+    anonymized_text.text = redact_money(anonymized_text.text)
+    return anonymized_text
 
-    anonymized_text = anonymizer.anonymize(text=text,analyzer_results=results)
-
-    return(anonymized_text)
